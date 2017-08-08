2b3eb01d173334baeee79ef30a331390fe55b657
migueref <reafmiguel@gmail.com>
2017-08-07 19:03:12 -0500

    Migrations created

--- database/migrations/2017_07_05_201612_create_transactions_table.php
+++ database/migrations/2017_07_05_201612_create_transactions_table.php
@@ -14,8 +14,13 @@ class CreateTransactionsTable extends Migration
     public function up()
     {
         Schema::create('transactions', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
+          $table->increments('id');
+          $table->increments('buyer_id')->unsigned();
+          $table->increments('product_id')->unsigned();
+          $table->timestamps();
+
+          $table->foreign('buyer_id')->references('id')->on('users');
+          $table->foreign('products_id')->references('id')->on('products');
         });
     }
 
